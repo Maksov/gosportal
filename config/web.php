45553f77dcc3669fc67a5a81bba1ce8fb4f750aa
Maksov <maksov4@gmail.com>
2015-06-13 19:37:15 +0500

    commit_3

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+	'language' => 'ru-RU',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -22,6 +23,15 @@ $config = [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
         ],
+		'i18n' => [
+        'translations' => [
+            'app*' => [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'basePath' => '@app/messages',
+                //'sourceLanguage' => 'en-US',
+            ],
+        ],
+		],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,18 +21,24 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
- <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
+
+<?php $this->beginBody() ?>
+    <div class="wrap">
+    
+
+        <div class="container">
+            
+			 <div class="row">
+			<?php
+			 NavBar::begin([
+                    'options' => [
+                    'class' => 'navbar-inverse',
                 ],
             ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
+			 echo Nav::widget([
+                'options' => ['class' => 'navbar-nav'],
                 'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
+                    ['label' => Yii::t('app','Home'), 'url' => ['/site/index']],
                     ['label' => 'About', 'url' => ['/site/about']],
                     ['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
@@ -42,11 +48,10 @@ AppAsset::register($this);
                             'linkOptions' => ['data-method' => 'post']],
                 ],
             ]);
-            NavBar::end();
-        ?>
-
-        <div class="container">
-            <?= Breadcrumbs::widget([
+          NavBar::end();
+			?>
+			</div>
+			<?= Breadcrumbs::widget([
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
             ]) ?>
             <?= $content ?>
